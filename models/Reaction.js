10e911bff494678a2e53ccb269e715fd2f0680ab
Chrisly <stlegerchrisly18@gmail.com>
2023-12-22 01:08:00 -0500

    models

---
+++ models/Reaction.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const dayjs = require("dayjs");
+
+const reactionSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    reactionId: {
+      type: mongoose.Schema.Types.ObjectId,
+      default: () => new mongoose.Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timeStamp) => {
+        return dayjs(timeStamp).format("MMM D YYYY [at] h:mm A");
+      },
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+module.exports = reactionSchema;
